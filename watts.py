c6309aacfe5543dd2dc0845b9d397c76460271fa
Stanton <ddstan@3c22fbcddebb.ant.amazon.com>
2021-04-21 16:46:46 -0400

    Uses a dict for all tCO2e rates.

--- watts.py
+++ watts.py
@@ -1,9 +1,37 @@
-def watts(mw, subregion):
-    if subregion == "AKGD":
-        co2 = 1045.0 * mw
-    elif subregion == "FRCC":
-        co2 = 936.1 * mw
-    else:
-        co2 = 952.9 * mw
+subregions = {
+	"AKGD": 1045.0,
+	"AKMS": 527.0,
+	"AZNM": 1027.5,
+	"CAMX": 498.7,
+	"ERCT": 936.1,
+	"FRCC": 936.1,
+	"HIMS": 1119.1,
+	"HIOA": 1682.6,
+	"MROE": 1689.7,
+	"MROW": 1249.2,
+	"NEWE": 527.6,
+	"NWPP": 643.4,
+	"NYCW": 597.8,
+	"NYLI": 1193.1,
+	"NYUP": 253.9,
+	"RFCE": 720.0,
+	"RFCM": 1321.2,
+	"RFCW": 1174.0,
+	"RMPA": 1281.9,
+	"SPNO": 1171.6,
+	"SPSO": 1172.8,
+	"SRMV": 858.4,
+	"SRMW": 1676.8,
+	"SRSO": 1033.5,
+	"SRTV": 1038.1,
+	"SRVC": 747.5,
+    "US": 952.9
+}
 
-    return co2
+def watts(mw, subregion):
+    if subregion in subregions:
+	    tco2e = subregions[subregion] * mw
+    else:
+    	tco2e = subregions["US"] * mw
+	
+    return tco2e
